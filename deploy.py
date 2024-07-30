d68c99fe278a6625ef8f121c33efbb9e4d7ba41f
junyoungparkdev <132217286+junyoungparkdev@users.noreply.github.com>
2024-07-30 21:52:24 +0900

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,18 +1,22 @@
 import os
-from prefect.deployments import DeploymentSpec
+from prefect import flow
+from prefect.deployments import Deployment
 from prefect.orion.schemas.schedules import CronSchedule
-from datetime import timedelta
-from Kafka_to_elasticsearch import kafka_to_elasticsearch_flow
+from kafka_to_elasticsearch_flow import kafka_to_elasticsearch_flow
 
-DeploymentSpec(
-    flow=kafka_to_elasticsearch_flow,
-    name="Kafka to Elasticsearch Deployment",
-    schedule=CronSchedule(cron="0 * * * *", timezone="Asia/Seoul"),
-    work_queue_name="default",
-    tags=["kafka", "elasticsearch"],
-    image={
-        "name": "team5/kafka-to-elasticsearch",
-        "tag": "0.1",
-        "dockerfile": "Dockerfile"
-    }
-)
+if __name__ == "__main__":
+    schedule = CronSchedule(
+        cron="0 * * * *",
+        timezone="Asia/Seoul"
+    )
+
+    deployment = Deployment.build_from_flow(
+        flow=kafka_to_elasticsearch_flow,
+        name="Kafka to Elasticsearch Deployment",
+        work_queue_name="default",
+        schedule=schedule,
+        tags=["kafka", "elasticsearch"]
+    )
+
+    deployment.apply()
+    print("Deployment created successfully.")
