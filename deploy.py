2cbd79b2ded5dda6b2860dcd4f0917c97462e3b8
junyoungparkdev <132217286+junyoungparkdev@users.noreply.github.com>
2024-07-30 22:51:50 +0900

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,19 +1,22 @@
 import os
-from prefect import flow
-from prefect.deployments import Deployment
-from prefect.orion.schemas.schedules import CronSchedule
 
-from kafka_to_elasticsearch_flow import kafka_to_elasticsearch_flow 
+from prefect import flow
+from prefect.deployments import DeploymentImage
+from prefect.client.schemas.schedules import CronSchedule
+
+from Kafka_to_elasticsearch import kafka_to_elasticsearch_flow
 
 if __name__ == "__main__":
-    Deployment.create(
-        flow=kafka_to_elasticsearch_flow,
-        name="Kafka to Elasticsearch Deployment",
-        work_queue_name="default",
-        schedule=CronSchedule(cron="0 * * * *", timezone="Asia/Seoul"),
-        image={
-            "name": "team5/kafka-to-elasticsearch",
-            "tag": "latest",
-            "pull_policy": "ALWAYS"
-        }
+    kafka_to_elasticsearch_flow.deploy(
+        name="jun_kaf2elk_deploy",
+        work_pool_name="docker-agent-pool",
+        work_queue_name="docker-agent",
+        image=DeploymentImage(
+            name="jun-kaf2elk",
+            tag="0.2",
+            dockerfile="Dockerfile",
+            platform="linux/arm64",
+        ),
+        schedule=(CronSchedule(cron="0 1 * * *", timezone="Asia/Seoul")),
+        build=True,
     )
