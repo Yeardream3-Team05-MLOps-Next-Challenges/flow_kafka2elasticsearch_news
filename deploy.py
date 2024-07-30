00c350bd277f419ecb73d61062658846f8bd418b
junyoungparkdev <132217286+junyoungparkdev@users.noreply.github.com>
2024-07-30 22:16:45 +0900

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,18 +1,19 @@
 import os
 from prefect import flow
-from prefect.deployments import DeploymentSpec
+from prefect.deployments import Deployment
 from prefect.orion.schemas.schedules import CronSchedule
 
 from kafka_to_elasticsearch_flow import kafka_to_elasticsearch_flow 
 
 if __name__ == "__main__":
-    DeploymentSpec(
+    Deployment.create(
         flow=kafka_to_elasticsearch_flow,
         name="Kafka to Elasticsearch Deployment",
         work_queue_name="default",
         schedule=CronSchedule(cron="0 * * * *", timezone="Asia/Seoul"),
         image={
             "name": "team5/kafka-to-elasticsearch",
-            "tag": "latest"
+            "tag": "latest",
+            "pull_policy": "ALWAYS"
         }
     )
