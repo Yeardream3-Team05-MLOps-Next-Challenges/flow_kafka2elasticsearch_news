bcf98dc173a59e28e52da8fd3a28a7d285a5375e
junyoungparkdev <132217286+junyoungparkdev@users.noreply.github.com>
2024-07-30 22:05:42 +0900

    Update Kafka_to_elasticsearch.py

--- Kafka_to_elasticsearch.py
+++ Kafka_to_elasticsearch.py
@@ -8,12 +8,17 @@ from prefect import flow, task
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+SERVER_HOST = os.getenv('SERVER_HOST')
+KAFKA_TOPIC = 'news_1'
+KAFKA_GROUP_ID = 'consumer-group1'
+BATCH_SIZE = 100  # Number of messages to process per batch
+
 @task
-def consume_kafka_data(server_host, kafka_topic, group_id):
+def consume_kafka_data():
     consumer = KafkaConsumer(
-        kafka_topic,
-        bootstrap_servers=[f'{server_host}:19094'],
-        group_id=group_id,
+        KAFKA_TOPIC,
+        bootstrap_servers=[f'{SERVER_HOST}:19094'],
+        group_id=KAFKA_GROUP_ID,
         auto_offset_reset='earliest',
         value_deserializer=lambda m: json.loads(m.decode('utf-8'))
     )
@@ -21,24 +26,24 @@ def consume_kafka_data(server_host, kafka_topic, group_id):
     try:
         for message in consumer:
             data.append(message.value)
-            if len(data) >= 100:  # BATCH_SIZE
+            if len(data) >= BATCH_SIZE:
                 break
     finally:
         consumer.close()
     return data
 
 @task
-def send_to_elasticsearch(data, server_host):
-    es = Elasticsearch([{'host': server_host, 'port': 19200}])
+def send_to_elasticsearch(data):
+    es = Elasticsearch([{'host': SERVER_HOST, 'port': 19200}])
     for record in data:
         record_id = sha256(json.dumps(record, sort_keys=True).encode()).hexdigest()
         if not es.exists(index="news", id=record_id):
             es.index(index="news", id=record_id, body=record)
 
 @flow
-def kafka_to_elasticsearch_flow(server_host, kafka_topic, group_id):
-    data = consume_kafka_data(server_host, kafka_topic, group_id)
-    send_to_elasticsearch(data, server_host)
+def kafka_to_elasticsearch_flow():
+    data = consume_kafka_data()
+    send_to_elasticsearch(data)
 
 if __name__ == "__main__":
-    kafka_to_elasticsearch_flow(os.getenv('SERVER_HOST'), 'news_1', 'consumer-group1')
+    kafka_to_elasticsearch_flow()
