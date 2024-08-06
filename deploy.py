a8ae8e6b23a25ab4aa42a1c00068b51ac80844dd
junyoungparkdev <132217286+junyoungparkdev@users.noreply.github.com>
2024-08-07 00:46:03 +0900

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -6,24 +6,24 @@ from prefect.client.schemas.schedules import CronSchedule
 
 from Kafka_to_elasticsearch import kafka_to_elasticsearch_flow
 
-if __name__ == "__main__":
-    kafka_to_elasticsearch_flow.deploy(
-        name="jun-kaf2elk",
-        work_pool_name="docker-agent-pool",
-        work_queue_name="docker-agent",
-        image=DeploymentImage(
-            name="jun-kaf2elk",
-            tag="0.1.6",
-            dockerfile="Dockerfile",
-            platform="linux/arm64",
-            buildargs={
-                        "PREFECT_API_URL": os.getenv("PREFECT_API_URL"),
-                        "PREFECT_DEFAULT_DOCKER_BUILD_NAMESPACE": os.getenv("PREFECT_DEFAULT_DOCKER_BUILD_NAMESPACE"),
-                        "SERVER_HOST": os.getenv("SERVER_HOST"),
-                        "KAFKA_TOPIC": os.getenv("KAFKA_TOPIC"),
-                        "KAFKA_URL": os.getenv("KAFKA_URL"),
-                        },
-        ),
-        schedule=(CronSchedule(cron="*/10 * * * *", timezone="Asia/Seoul")),
-        build=True,
-    )
+# if __name__ == "__main__":
+#     kafka_to_elasticsearch_flow.deploy(
+#         name="jun-kaf2elk",
+#         work_pool_name="docker-agent-pool",
+#         work_queue_name="docker-agent",
+#         image=DeploymentImage(
+#             name="jun-kaf2elk",
+#             tag="0.1.6",
+#             dockerfile="Dockerfile",
+#             platform="linux/arm64",
+#             buildargs={
+#                         "PREFECT_API_URL": os.getenv("PREFECT_API_URL"),
+#                         "PREFECT_DEFAULT_DOCKER_BUILD_NAMESPACE": os.getenv("PREFECT_DEFAULT_DOCKER_BUILD_NAMESPACE"),
+#                         "SERVER_HOST": os.getenv("SERVER_HOST"),
+#                         "KAFKA_TOPIC": os.getenv("KAFKA_TOPIC"),
+#                         "KAFKA_URL": os.getenv("KAFKA_URL"),
+#                         },
+#         ),
+#         schedule=(CronSchedule(cron="*/10 * * * *", timezone="Asia/Seoul")),
+#         build=True,
+#     )
