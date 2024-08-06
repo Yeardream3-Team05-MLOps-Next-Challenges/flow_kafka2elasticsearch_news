2da2d6dcae2d7cf45ee80aeeed05c1b6df96b46c
junyoungparkdev <132217286+junyoungparkdev@users.noreply.github.com>
2024-08-06 20:23:14 +0900

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -7,16 +7,23 @@ from prefect.client.schemas.schedules import CronSchedule
 from Kafka_to_elasticsearch import kafka_to_elasticsearch_flow
 
 if __name__ == "__main__":
-    kafka_to_elasticsearch_flow.deploy(
-        name="jun_kaf2elk_deploy",
+    hun_fetch_and_send_stock_flow.deploy(
+        name="jun-kaf2elk",
         work_pool_name="docker-agent-pool",
         work_queue_name="docker-agent",
         image=DeploymentImage(
             name="jun-kaf2elk",
-            tag="0.2",
+            tag="0.1.6",
             dockerfile="Dockerfile",
             platform="linux/arm64",
+            buildargs={
+                        "PREFECT_API_URL": os.getenv("PREFECT_API_URL"),
+                        "DOCKER_NAMESPACE": os.getenv("DOCKER_NAMESPACE"),
+                        "SERVER_HOST": os.getenv("SERVER_HOST"),
+                        "KAFKA_TOPIC": os.getenv("KAFKA_TOPIC"),
+                        "KAFKA_URL": os.getenv("KAFKA_URL"),
+                        },
         ),
-        schedule=(CronSchedule(cron="0 1 * * *", timezone="Asia/Seoul")),
+        schedule=(CronSchedule(cron="0 8 * * *", timezone="Asia/Seoul")),
         build=True,
     )
