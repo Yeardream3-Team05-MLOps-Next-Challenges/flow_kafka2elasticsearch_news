0957251e15ea842ff94d7175bb8199c4977e2f94
junyoungparkdev <132217286+junyoungparkdev@users.noreply.github.com>
2024-09-03 21:35:52 +0900

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,3 +1,50 @@
+# import os
+# from prefect import flow
+# from prefect.deployments import DeploymentImage
+# from prefect.client.schemas.schedules import CronSchedule
+# from prefect.tasks import task_input_hash
+# from Kafka_to_elasticsearch import kafka_to_elasticsearch_flow
+
+# @flow(
+#     name="kafka_to_elasticsearch_main_flow",
+#     version="0.1.6",
+#     description="Main flow to control Kafka to Elasticsearch data transfer",
+#     retries=3,
+#     retry_delay_seconds=300,
+# )
+# def main_flow():
+#     kafka_to_elasticsearch_flow()
+
+# if __name__ == "__main__":
+#     main_flow.deploy(
+#         name="jun-kaf2elk",
+#         work_pool_name="docker-agent-pool",
+#         work_queue_name="docker-agent",
+#         image=DeploymentImage(
+#             name="jun-kaf2elk",
+#             tag="0.1.6",
+#             dockerfile="Dockerfile",
+#             platform="linux/arm64",
+#             buildargs={
+#                 "PREFECT_API_URL": os.getenv("PREFECT_API_URL"),
+#                 "PREFECT_DEFAULT_DOCKER_BUILD_NAMESPACE": os.getenv("PREFECT_DEFAULT_DOCKER_BUILD_NAMESPACE"),
+#                 "SERVER_HOST": os.getenv("SERVER_HOST"),
+#                 "KAFKA_TOPIC": os.getenv("KAFKA_TOPIC"),
+#                 "KAFKA_URL": os.getenv("KAFKA_URL"),
+#             },
+#         ),
+#         schedule=(CronSchedule(cron="0,10,20,30,40,50 * * * *", timezone="Asia/Seoul")),
+#         build=True,
+#         job_variables={
+#             "PREFECT__FLOW_SETTINGS__concurrency_limit": 1,
+#             "PREFECT__FLOW_SETTINGS__concurrency_queue": "jun-kaf2elk-queue",
+#         },
+#     )
+
+
+
+
+========== 
 import os
 
 from prefect import flow
@@ -26,8 +73,19 @@ if __name__ == "__main__":
         ),
         schedule=(CronSchedule(cron="0 13 * * *", timezone="Asia/Seoul")),
         build=True,
+        job_variables={
+            "PREFECT__FLOW_SETTINGS__concurrency_limit": 1,
+            "PREFECT__FLOW_SETTINGS__concurrency_queue": "jun-kaf2elk-queue",
+        }
     )
 
+
+
+
+
+
+
+# =============
 # import os
 # from prefect import flow
 # from prefect.deployments import Deployment
