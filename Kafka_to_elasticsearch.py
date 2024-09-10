16eb36cce60286f203a92314db1b8999fe6db1b9
junyoungparkdev <132217286+junyoungparkdev@users.noreply.github.com>
2024-09-10 20:01:12 +0900

    Update Kafka_to_elasticsearch.py

--- Kafka_to_elasticsearch.py
+++ Kafka_to_elasticsearch.py
@@ -44,6 +44,7 @@ def consume_kafka_data():
         group_id=KAFKA_GROUP_ID,
         auto_offset_reset='earliest',
         value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+        max_poll_interval_ms=300000
         request_timeout_ms=20000,  # 20초
         session_timeout_ms=10000   # 10초
     )
@@ -64,73 +65,34 @@ def consume_kafka_data():
 @task
 def send_to_elasticsearch(data):
     es = Elasticsearch([{'host': SERVER_HOST, 'port': 19200,'scheme': 'http'}])
+
+    if not es.indices.exists(index="news"):
+        es.indices.create(index="news", body=news_mapping)
     
     actions = []
     for record in data:
         actions.append({
+            "_op_type": "index"
             "_index": "news",
+            "_id": record['url']
             "_source": record
         })
     
     if actions:
-        success, _ = bulk(es, actions)
-        logging.info(f"Inserted {success} documents into Elasticsearch")
+        try:
+            success, failed = bulk(es, actions)
+            logging.info(f"Inserted {success} documents into Elasticsearch, Failed {failed} documents")
+        except Exception as e:
+            logging.error(f"Error during bulk operation: {e}")
+    es.close()
 
 @flow
 def kafka_to_elasticsearch_flow():
     data = consume_kafka_data()
-    send_to_elasticsearch(data)
+    if data:
+        send_to_elasticsearch(data)
+    else:
+        logging.info("No new data to process")
 
 if __name__ == "__main__":
     kafka_to_elasticsearch_flow()
-
-
-# import json
-# import os
-# from hashlib import sha256
-# import logging
-# from kafka import KafkaConsumer, errors as kafka_errors
-# from elasticsearch import Elasticsearch, exceptions as es_exceptions
-# from prefect import flow, task
-
-# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# SERVER_HOST = os.getenv('SERVER_HOST')
-# KAFKA_TOPIC = 'news_1'
-# KAFKA_GROUP_ID = 'consumer-group1'
-# BATCH_SIZE = 4000
-
-# @task
-# def consume_kafka_data():
-#     consumer = KafkaConsumer(
-#         KAFKA_TOPIC,
-#         bootstrap_servers=[f'{SERVER_HOST}:19094'],
-#         group_id=KAFKA_GROUP_ID,
-#         auto_offset_reset='earliest',
-#         value_deserializer=lambda m: json.loads(m.decode('utf-8'))
-#     )
-#     data = []
-#     try:
-#         for message in consumer:
-#             data.append(message.value)
-#             if len(data) >= BATCH_SIZE:
-#                 break
-#     finally:
-#         consumer.close()
-#     return data
-
-# @task
-# def send_to_elasticsearch(data):
-#     es = Elasticsearch([{'host': SERVER_HOST, 'port': 19200}])
-#     for record in data:
-#         record_id = sha256(json.dumps(record, sort_keys=True).encode()).hexdigest()
-#         if not es.exists(index="news", id=record_id):
-#             es.index(index="news", id=record_id, body=record)
-
-# @flow
-# def kafka_to_elasticsearch_flow():
-#     data = consume_kafka_data()
-#     send_to_elasticsearch(data)
-
-# if __name__ == "__main__":
-#     kafka_to_elasticsearch_flow()
